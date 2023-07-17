0006330b4c0d6bfe2ee96eefa186f519df031acc
Andrew Njoo <adnjoo@gmail.com>
2023-07-17 11:38:40 -0700

    edit copy

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Example() {
                 <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  src='/books.png'
                   alt=""
                 />
               </a>
@@ -115,10 +115,10 @@ export default function Example() {
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Keep track of your reading with bookkss
+              The more you learn, the more you earn..
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Bookkss helps you keep track of your reading, and book reviews
+              Bookkss keeps track of your reading, and book reviews for future reference
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -126,9 +126,6 @@ export default function Example() {
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
