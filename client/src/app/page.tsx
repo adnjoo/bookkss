2a2a8cccfae9754dae905d30bcd5aed4688df4d8
Andrew Njoo <adnjoo@gmail.com>
2023-07-19 08:48:43 -0700

    remove bootstrap

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,15 @@
-export default function Home() {
+import { getServerSession } from 'next-auth/next';
+
+import { authOptions } from '@/lib/auth';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/dashboard');
+  }
+
   return (
     <div>
       <div className='mt-40'>
--- client/src/app/signin/page.tsx
+++ client/src/app/signin/page.tsx
@@ -1,29 +1,11 @@
 'use client';
 
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
 export default function SignIn() {
   return (
     <div className='mx-4 mt-12 flex justify-center'>
-      <Form className='flex flex-col rounded-xl border p-4 sm:w-[400px]'>
-        <Form.Group className='mb-3' controlId='formBasicEmail'>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type='email' placeholder='Enter email' />
-        </Form.Group>
-
-        <Form.Group className='mb-3' controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Password' />
-        </Form.Group>
-        <Form.Group className='mb-3' controlId='formBasicCheckbox'></Form.Group>
-        <Button className='bg-slate-700' variant='secondary' type='submit'>
-          Submit
-        </Button>
-        <div className='mt-4'>
-          <a href='/api/auth/signin'>Continue with Google</a>
-        </div>
-      </Form>
+      <div className='mt-4'>
+        <a href='/api/auth/signin'>Continue with Google</a>
+      </div>
     </div>
   );
 }
