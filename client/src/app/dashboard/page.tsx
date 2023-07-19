a5336911f791af154a9734ac72a6b7bff5a33d98
Andrew Njoo <adnjoo@gmail.com>
2023-07-18 19:02:59 -0700

    move auth to lib

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from 'next-auth/next';
 import { redirect } from 'next/navigation';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+
+import { authOptions } from '@/lib/auth';
 
 const ServerProtectedPage = async () => {
   const session = await getServerSession(authOptions);
