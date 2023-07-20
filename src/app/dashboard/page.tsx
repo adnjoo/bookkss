3ba3d532c02aa6f2de6c712317ca463353073009
Andrew Njoo <adnjoo@gmail.com>
2023-07-19 22:45:05 -0700

    18: post review (#19)
    * post review
    * clear content button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,31 +7,85 @@ import { useEffect, useState } from 'react';
 const ServerProtectedPage = () => {
   const { data: session } = useSession();
   const [reviews, setReviews] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const getReviews = async () => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/get-reviews`
+    );
+    setReviews(res.data);
+  };
 
   useEffect(() => {
+    getReviews();
+  }, []);
+
+  const onAddReview = () => {
     axios
-      .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/get-reviews`)
+      .post(`${process.env.NEXT_PUBLIC_SERVER_URL}/add-review`, {
+        title,
+        body,
+      })
       .then((res) => {
         console.log(res.data);
-        setReviews(res.data);
+        getReviews();
+        setTitle('');
+        setBody('');
       });
-  }, []);
+  };
 
   return (
     <section className='py-24'>
       <div className='container mx-auto flex flex-col px-4'>
-        <h1 className='text-2xl font-bold'>
-          This is a server-side protected page
-        </h1>
+        <h1 className='text-2xl font-bold'>Welcome to your dashboard</h1>
         <h2 className='mt-4 font-medium'>
           You are logged in as: {session?.user?.name}
         </h2>
+        <div className='mt-4 flex flex-col rounded-xl border p-4'>
+          <div className='mb-2 underline'>Add review</div>
+          <div>
+            <div className='mb-2'>Title</div>
+            <input
+              className='w-full border p-4'
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+          </div>
+          <div>
+            <div className='mb-2'>Body</div>
+            <textarea
+              className='w-full border p-6'
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+            />
+          </div>
+          <div className='flex gap-2'>
+            <button
+              className='mt-4 w-[100px] rounded bg-gray-500 p-2 text-white'
+              onClick={onAddReview}
+            >
+              Add review
+            </button>
+            <button
+              className='mt-4 w-[100px] rounded bg-gray-700 p-2 text-white'
+              onClick={() => {
+                setTitle('');
+                setBody('');
+              }}
+            >
+              Clear content
+            </button>
+          </div>
+        </div>
         <div className='mt-4'>
-          Reviews
+          <div className='mb-2'>Reviews</div>
           {reviews.map((review: any) => (
-            <div key={review.id}>
-              <h3>{review.title}</h3>
-              <p>{review.body}</p>
+            <div key={review.id} className='mb-4 rounded border p-4'>
+              <h3 className='text-xl font-bold'>
+                {review.title} {new Date(review.createdAt).toLocaleDateString()}
+              </h3>
+              <p className='mt-2'>{review.body}</p>
             </div>
           ))}
         </div>
