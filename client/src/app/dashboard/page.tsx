e313c976977dbb38a887d30de5b2a7afe1d335dd
Andrew Njoo <adnjoo@gmail.com>
2023-08-03 08:40:37 -0700

    edit dashboard top margin

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -90,7 +90,7 @@ const ServerProtectedPage = () => {
   };
 
   return (
-    <section className='py-24'>
+    <section className='py-12'>
       <div className='container mx-auto flex flex-col px-4'>
         <h1 className='text-2xl font-bold'>Welcome to your dashboard</h1>
         <h2 className='mt-4 font-medium'>
