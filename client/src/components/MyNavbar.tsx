c4ec5f41bb2043bfcc4c8f013dc71e8315d90aa7
Andrew Njoo <adnjoo@gmail.com>
2023-08-05 14:12:16 -0700

    edit navbar

--- client/src/components/MyNavbar.tsx
+++ client/src/components/MyNavbar.tsx
@@ -1,28 +1,52 @@
-import { getServerSession } from 'next-auth/next';
+'use client';
 
-import { authOptions } from '../lib/auth';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { AiOutlineMenu } from 'react-icons/ai';
 
-export const MyNavbar = async () => {
-  const session = await getServerSession(authOptions);
+export const MyNavbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const { data: session } = useSession();
   return (
     <>
       {session ? (
-        <nav className='mx-4 mt-6 flex justify-between gap-4 lg:mx-60'>
-          <div className='flex gap-4'>
-            <img src='/logo-long.png' className='w-24' />
+        <>
+          <nav className='mx-4 mt-6 flex justify-between gap-4 lg:mx-60'>
+            <div className='flex gap-4'>
+              <img src='/logo-long.png' className='hidden w-24 sm:block' />
+              <img src='/logo.png' className='w-8 sm:hidden' />
+              <div className='hidden gap-4 sm:flex'>
+                <a href='/'>Home</a>
+                <a href='/dashboard'>Dashboard</a>
+                <a href='/discover'>Discover</a>
+              </div>
+            </div>
+
+            <AiOutlineMenu
+              className='cursor-pointer sm:hidden'
+              onClick={() => setShowMenu(!showMenu)}
+            />
+            <a href='/api/auth/signout' className='hidden sm:flex'>
+              Sign out
+            </a>
+          </nav>
+          <div
+            className={`ml-4 mt-2 flex gap-4 ${
+              showMenu ? 'flex-col' : 'hidden'
+            }`}
+          >
             <a href='/'>Home</a>
             <a href='/dashboard'>Dashboard</a>
             <a href='/discover'>Discover</a>
-          </div>
-          <div>
             <a href='/api/auth/signout'>Sign out</a>
           </div>
-        </nav>
+        </>
       ) : (
         <nav className='mx-4 mt-6 flex justify-between gap-4 lg:mx-60'>
           <div className='flex gap-4'>
-            <img src='/logo-long.png' className='w-24' />
-            <a href='/'>Home</a>
+            <a href='/'>
+              <img src='/logo-long.png' className='w-24' />
+            </a>
             <a href='/discover'>Discover</a>
           </div>
           <div>
