bc986aa1e46123fdd3c79d8c981b787a5cfcd805
Andrew Njoo <adnjoo@gmail.com>
2023-08-06 15:55:29 -0700

    update navbar

--- client/src/components/MyNavbar.tsx
+++ client/src/components/MyNavbar.tsx
@@ -11,12 +11,14 @@ export const MyNavbar = async () => {
         <nav className='mx-4 mt-6 flex justify-between gap-4 lg:mx-60'>
           <div className='flex items-center gap-4'>
             <a href='/'>
-              <img src='/logo-long.png' className='w-24' />
+              <img src='/logo-long.png' className='hidden w-24 sm:flex' />
+              <img src='/logo.png' className='flex w-12 sm:hidden' />
             </a>
             {/* <a href='/dashboard'>Home</a> */}
+            <a href='/archive'>Archive</a>
             <a href='/discover'>Discover</a>
           </div>
-          <a href='/api/auth/signout'>
+          <a href='/api/auth/signout' className='flex items-center'>
             <AiOutlineLogout className='flex h-6 w-6 sm:hidden' />
             <span className='hidden sm:flex'>Sign out</span>
           </a>
