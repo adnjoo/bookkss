e0a7aadb0fd2b29dd705315e838173932ecf1cf5
Andrew Njoo <adnjoo@gmail.com>
2023-08-16 09:57:36 -0700

    remove quotes from seuss quote

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -84,8 +84,8 @@ function App() {
               />
             </p>
             <p className='my-4 text-lg text-gray-600'>
-              "The more that you read, the more things you will know. The more
-              that you learn, the more places you'll go." - Dr. Seuss (Author)
+              The more that you read, the more things you will know. The more
+              that you learn, the more places you'll go. - Dr. Seuss (Author)
               <img
                 src='/seuss.jpg'
                 alt='seuss'
