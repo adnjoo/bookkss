e9f1358e8c9c9d6ef61c5ec4fcca4ee2381ab5b7
Andrew Njoo <adnjoo@gmail.com>
2023-08-20 09:21:16 -0700

    edit footer copy

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,45 +1,45 @@
-import { Link } from "react-router-dom";
-import { AiFillLinkedin } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import { AiFillLinkedin } from 'react-icons/ai';
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-8 text-white">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-between">
+    <footer className='bg-gray-800 py-8 text-white'>
+      <div className='container mx-auto px-4'>
+        <div className='flex justify-between'>
           <div>
-            <Link to="/">
-              <h3 className="text-lg font-bold">bookkss</h3>
+            <Link to='/'>
+              <h3 className='text-lg font-bold'>bookkss</h3>
             </Link>
-            <p className="mt-2 hidden sm:flex">
-              Helping you write and share book reviews.
+            <p className='mt-2 hidden sm:flex'>
+              Helping readers write and share book reviews.
             </p>
-            <Link to="/about-us" className="mt-2">
+            <Link to='/about-us' className='mt-2'>
               About Us
             </Link>
           </div>
           <div>
-            <h3 className="text-lg font-bold">Links</h3>
-            <ul className="mt-2 flex flex-col">
-              <span className="mb-1">
-                <Link to="/">Home</Link>
+            <h3 className='text-lg font-bold'>Links</h3>
+            <ul className='mt-2 flex flex-col'>
+              <span className='mb-1'>
+                <Link to='/'>Home</Link>
               </span>
-              <span className="mb-1">
-                <Link to="/dashboard">Dashboard</Link>
+              <span className='mb-1'>
+                <Link to='/dashboard'>Dashboard</Link>
               </span>
-              <span className="mb-1">
-                <Link to="/discover">Discover</Link>
+              <span className='mb-1'>
+                <Link to='/discover'>Discover</Link>
               </span>
             </ul>
           </div>
           <div>
-            <h3 className="text-lg font-bold">Follow Us</h3>
-            <ul className="mt-2">
-              <span className="mb-1">
+            <h3 className='text-lg font-bold'>Follow Us</h3>
+            <ul className='mt-2'>
+              <span className='mb-1'>
                 <a
-                  href="https://linkedin.com/company/bookkssco"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white hover:text-gray-400 transition duration-150 ease-in-out"
+                  href='https://linkedin.com/company/bookkssco'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='text-white transition duration-150 ease-in-out hover:text-gray-400'
                 >
                   <AiFillLinkedin size={24} />
                 </a>
@@ -47,8 +47,8 @@ export const Footer = () => {
             </ul>
           </div>
         </div>
-        <hr className="my-4 border-gray-700" />
-        <p className="text-center text-sm">
+        <hr className='my-4 border-gray-700' />
+        <p className='text-center text-sm'>
           &copy; {new Date().getFullYear()} Bookkss. All rights reserved.
         </p>
       </div>
