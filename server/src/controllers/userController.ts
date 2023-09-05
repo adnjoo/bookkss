cde4b0a5c5ccd00ada6a1fecbc9a3d66b923a55d
Andrew Njoo <adnjoo@gmail.com>
2023-09-04 21:26:32 -0700

    139: profile images, server

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -2,9 +2,20 @@ import { Router, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
+import multer from 'multer';
+import { createClient } from '@supabase/supabase-js';
 
 const router = Router();
 const prisma = new PrismaClient();
+
+// Multer setup for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const supabase = createClient(
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_KEY as string
+);
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -130,6 +141,63 @@ export const isAuth = async (req: Request, res: Response) => {
       return res.status(401).json({ message: 'Token has expired' });
     }
 
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const uploadProfilePicture = async (req: any, res: Response) => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({ message: 'Not authenticated' });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET as string) as {
+      userId: number;
+    };
+
+    if (!decodedToken) {
+      return res.status(401).json({ message: 'Not authenticated' });
+    }
+
+    // Handle file upload
+    upload.single('profilePicture')(req, res, async (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(400).json({ message: 'Error uploading file' });
+      }
+
+      const file = req.file; // req.file contains the uploaded file
+
+      if (!file) {
+        return res
+          .status(400)
+          .json({ message: 'Profile picture not provided' });
+      }
+
+      // Generate a unique filename for the uploaded image (you can use a UUID library)
+      const fileName = `${decodedToken.userId}_avatar.jpg`;
+
+      // Upload the image to Superbase storage
+      const { data, error } = await supabase.storage
+        .from('avatars')
+        .upload(fileName, file.buffer); // Use file.buffer to access the file data
+
+      if (error) {
+        console.error(error);
+        return res
+          .status(500)
+          .json({ message: 'Error uploading profile picture' });
+      }
+
+      // Optionally, you can save the filename in your database for the user
+      // Update the user's profile with the new profile picture filename in your Prisma code here
+
+      return res.status(200).json({ message: 'Profile picture uploaded' });
+    });
+  } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
   }
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 
-import { register, login, isAuth } from '../controllers/userController';
+import {
+  register,
+  login,
+  isAuth,
+  uploadProfilePicture,
+} from '../controllers/userController';
 
 export const userRouter = express.Router();
 
 userRouter.post('/register', register);
 userRouter.post('/login', login);
 userRouter.get('/is-auth', isAuth);
+userRouter.post('/upload-profile-picture', uploadProfilePicture);
